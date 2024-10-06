a21a341b34d1a5abaa83ca90f3c6cee42b174909
Oleg Ershov <oershov@kameleoon.com>
2024-10-06 17:02:30 +0400

    feat: add spawn module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { spawn } from 'child_process';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn('node', [join(__dirname, 'files', 'script.js'), ...args], {
+    stdio: ['pipe', 'pipe', 'pipe'],
+  });
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(['1', '2', '33', '55', '88', '180',]);
